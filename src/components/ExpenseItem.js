c8ec2ece0e39ae06688661cc8f0215d015301748
Hans Schroeder <hajschroeder@gmail.com>
2023-04-08 13:31:56 -0700

    include styling, custom component, JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,15 @@
+import './ExpenseItem.css'
 function ExpenseItem () {
+
+  const expenseDate = new Date(2023, 3, 8);
+  const expenseTitle = 'HardCoded Title';
+  const expenseAmount = 'Hard Coded Amount';
   return (
-    <div>
-      <div>Date</div>
-      <div>
-        <h2>Title</h2>
-        <div>Amount</div>
+    <div className="expense-item"> 
+      <div>{expenseDate.toDateString()}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>{expenseAmount}</div>
       </div>
     </div>
   ) 

106e17043e1707a24837be68d2f67fd5f06f1247
Hans Schroeder <hajschroeder@gmail.com>
2023-04-09 11:10:41 -0700

    include props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,38 @@
 import './ExpenseItem.css'
-function ExpenseItem () {
 
-  const expenseDate = new Date(2023, 3, 8);
-  const expenseTitle = 'HardCoded Title';
-  const expenseAmount = 'Hard Coded Amount';
+function ExpenseItem (props) {
+
+
   return (
     <div className="expense-item"> 
-      <div>{expenseDate.toDateString()}</div>
+      <div>{props.date.toDateString()}</div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>{props.amount}</div>
       </div>
     </div>
   ) 
 }
 
 export default ExpenseItem
+// Below is the code before using 'props'
+
+// import './ExpenseItem.css'
+
+// function ExpenseItem () {
+
+//   const expenseDate = new Date(2023, 3, 8);
+//   const expenseTitle = 'HardCoded Title';
+//   const expenseAmount = 'Hard Coded Amount';
+//   return (
+//     <div className="expense-item"> 
+//       <div>{expenseDate.toDateString()}</div>
+//       <div className='expense-item__description'>
+//         <h2>{expenseTitle}</h2>
+//         <div className='expense-item__price'>{expenseAmount}</div>
+//       </div>
+//     </div>
+//   ) 
+// }
+
+// export default ExpenseItem

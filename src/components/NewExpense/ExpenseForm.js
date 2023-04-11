8e5c9f01375c140a374b372708bbc48e1f0a3e81
Hans Schroeder <hajschroeder@gmail.com>
2023-04-10 18:43:42 -0700

    include clear form functionality

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -81,13 +81,16 @@ const ExpenseForm = () => {
       date: new Date(enteredDate)
     };
     console.log(expenseData)
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls ">
         <div className="new-expense__control ">
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler} />
+          <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
         </div>
         {/* -The following was some test code for demonstation purposes. 
         -In opening the Dev tools, after this test function executes the console will give us an object full of data. 
@@ -102,11 +105,11 @@ const ExpenseForm = () => {
         </div> */}
         <div className="new-expense__control ">
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
+          <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} value={enteredAmount}/>
         </div>
         <div className="new-expense__control ">
           <label>Date</label>
-          <input type='date' min="2019-01-01" max="2024-12-12" onChange={dateChangeHandler} />
+          <input type='date' min="2019-01-01" max="2024-12-12" onChange={dateChangeHandler} value={enteredDate}/>
         </div>
 
       </div>

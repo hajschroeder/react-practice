014a18fb34ea39b8f4c71e554030c480eb8f6c4f
Hans Schroeder <hajschroeder@gmail.com>
2023-04-14 11:59:09 -0700

    include functionality to update list

--- src/App.js
+++ src/App.js
@@ -1,42 +1,77 @@
+import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+
+const DUMMY_EXPENSES = [
+  {
+    itemNumber: 1,
+    title: 'EXPENSE ONE',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+
+  },
+  { itemNumber: 2, 
+    title: 'EXPENSE TWO', 
+    amount: 799.49, 
+    date: new Date(2021, 2, 12) 
+  },
+  {
+    itemNumber: 3,
+    title: 'EXPENSE THREE',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    itemNumber: 4,
+    title: 'EXPENSE FOUR',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      itemNumber: 1,
-      title: 'EXPENSE ONE',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
+  const[ expenseState, setExpenseState] =useState(DUMMY_EXPENSES)
+
+
+  // const expenses = [
+  //   {
+  //     itemNumber: 1,
+  //     title: 'EXPENSE ONE',
+  //     amount: 94.12,
+  //     date: new Date(2020, 7, 14),
   
-    },
-    { itemNumber: 2, 
-      title: 'EXPENSE TWO', 
-      amount: 799.49, 
-      date: new Date(2021, 2, 12) 
-    },
-    {
-      itemNumber: 3,
-      title: 'EXPENSE THREE',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      itemNumber: 4,
-      title: 'EXPENSE FOUR',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  //   },
+  //   { itemNumber: 2, 
+  //     title: 'EXPENSE TWO', 
+  //     amount: 799.49, 
+  //     date: new Date(2021, 2, 12) 
+  //   },
+  //   {
+  //     itemNumber: 3,
+  //     title: 'EXPENSE THREE',
+  //     amount: 294.67,
+  //     date: new Date(2021, 2, 28),
+  //   },
+  //   {
+  //     itemNumber: 4,
+  //     title: 'EXPENSE FOUR',
+  //     amount: 450,
+  //     date: new Date(2021, 5, 12),
+  //   },
+  // ];
 
   const addExpenseHandler = expense => {
-    console.log('app.js')
-    console.log(expense)
+    setExpenseState(prevExpenses => {
+      return [expense, ...prevExpenses]
+    });
+    // console.log('app.js')
+    // console.log(expense)
   }
   return (
     <div>
       <div>
         <NewExpense onAddExpense={addExpenseHandler}/>
-        <Expenses itemsDef={expenses}/>
+        <Expenses itemsDef={expenseState}/>
       </div>
 
     </div>

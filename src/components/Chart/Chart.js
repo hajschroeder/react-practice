0a275ba8770c74fc482804f11ea1af325275d13d
Hans Schroeder <hajschroeder@gmail.com>
2023-04-15 13:32:09 -0700

    include components for chart

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,8 +1,9 @@
 import './Chart.css'
+import ChartBar from './ChartBar'
 const Chart = (props) => {
   return (
     <div className="chart">
-      {}
+      {props.dataPoints.map(dataPoint => <ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={null} label={dataPoint.label}/>)}
     </div>
   )
 }
